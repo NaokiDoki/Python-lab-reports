a0a1499d1a5ac6c4bf51143b36ebd41984602078
Prasiddha Grg <103157685+NaokiDoki@users.noreply.github.com>
2024-06-12 15:45:03 +0545

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,7 @@
+# Write a program to print your name,gender,DOB with user input
+
+name = input("Enter your name: ")
+gen = input("Enter your gender: ")
+dob = input("Enter date of birth: ")
+
+print(f'\nName: {name}\nGender: {gen}\nDOB: {dob}')
