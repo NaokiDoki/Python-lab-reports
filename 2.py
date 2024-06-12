97526a62ae3757c57ed55ba318e969304a176f25
Prasiddha Grg <103157685+NaokiDoki@users.noreply.github.com>
2024-06-12 07:12:15 +0545

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,7 @@
+"""Write a Program to find the sum of two integers numbers with
+user input and display the result"""
+
+x = input("Enter a number: ")
+y = input("Enter a number: ")
+sum = int(x) + int(y)
+print(f'sum = {sum}')
